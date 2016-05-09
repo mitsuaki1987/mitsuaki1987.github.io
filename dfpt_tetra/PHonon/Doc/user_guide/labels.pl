6790aae68010adb1f43c496eaf528389e2a6dcf3
Mitsuaki Kawamura <kawamitsuaki@gmail.com>
2016-05-09 19:46:37 +0900

    Add manuals for dfpt-tetra

---
+++ dfpt_tetra/PHonon/Doc/user_guide/labels.pl
@@ -0,0 +1,29 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/Sec:para/;
+$external_labels{$key} = "$URL/" . q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2008 (1.71)
+# labels from external_latex_labels array.
+
+
+$key = q/_newlabelxx/;
+$external_latex_labels{$key} = q|\ifx|; 
+$noresave{$key} = "$nosave";
+
+$key = q//;
+$external_latex_labels{$key} = q|\fi|; 
+$noresave{$key} = "$nosave";
+
+$key = q/Sec:para/;
+$external_latex_labels{$key} = q|5|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ dfpt_tetra/PP/Doc/user_guide/images.pl
@@ -0,0 +1,48 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/phi;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="16" HEIGHT="35" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$ \phi$">|; 
+
+$key = q/sum_{{i=1}}^{{n_s}};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="44" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="$ \sum_{{i=1}}^{{n_s}}$">|; 
+
+$key = q/theta;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="14" HEIGHT="20" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$ \theta$">|; 
+
+$key = q/rangle;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="12" HEIGHT="39" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img8.png"
+ ALT="$ \rangle$">|; 
+
+$key = q/langle;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="12" HEIGHT="39" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="$ \langle$">|; 
+
+$key = q/varphi_{{nk}}^{};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="33" HEIGHT="35" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="$ \varphi_{{n k}}^{}$">|; 
+
+$key = q/varphi_{{{{rm{index}(i)}}^{{{rm{atom}};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="65" HEIGHT="38" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img6.png"
+ ALT="$ \varphi_{{{\rm index}(i)}}^{{\rm atom}}$">|; 
+
+1;
+
